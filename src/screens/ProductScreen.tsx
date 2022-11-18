f88ce812b4f3ee1a0e2c17b1b9efccdd566f6b7b
Semi-dios <tipouno@misena.edu.co>
2022-11-18 13:28:28 -0500

    feat/View_product/create_and_development_new_screen_about_single_product

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,6 +1,13 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import * as React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import {ProductsStackParmas} from '../navigator/ProductsNavigator';
 
 interface Props
@@ -16,8 +23,35 @@ export const ProductScreen = ({route, navigation}: Props) => {
   }, []);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{id}Products Screen</Text>
-      <Text style={styles.title}>{name}</Text>
+      <ScrollView>
+        <Text style={styles.label}>Name Product</Text>
+        <TextInput placeholder="Name product" style={styles.textInput} />
+        <Text style={styles.label}>Select Category</Text>;{/*Picker*/}
+        <TouchableOpacity
+          onPress={() => {}}
+          style={{backgroundColor: '#5856d6'}}>
+          <Text style={{...styles.title, color: 'white'}}>Create</Text>;
+        </TouchableOpacity>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginTop: 10,
+          }}>
+          <TouchableOpacity
+            onPress={() => {}}
+            style={{backgroundColor: '#5856d6'}}>
+            <Text style={{...styles.title, color: 'white'}}>
+              Take a picture
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            style={{backgroundColor: '#5856d6'}}>
+            <Text style={{...styles.title, color: 'white'}}>Upload local</Text>;
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -25,11 +59,27 @@ export const ProductScreen = ({route, navigation}: Props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 10,
+    marginHorizontal: 20,
     justifyContent: 'center',
     alignItems: 'center',
   },
   title: {
     fontSize: 20,
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  textInput: {
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 20,
+    borderColor: 'rgba(0,0,0,0.2)',
+    height: 45,
+    maeginTop: 15,
     marginBottom: 20,
   },
 });
